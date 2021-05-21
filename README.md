364508989dfdd6cad129da2f958679ec9aab90a7
Mariama MANE <mariama.mane5@etu.univ-lorraine.fr>
2021-05-21 22:02:48 +0200

    Le projet de réseau serveur pour les sites

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # ProjetReseau_M1_MIAGE_DENIS_MANE_LAHSSINI
+
+README Projet réseau - M1 Miage - MANE Mariama, LAHSSINI Asma, DENIS Melvyn 
+
+Ce que fait le projet :
+Le projet sert à créer un serveur qui est fonctionnel et à afficher des sites fournis sur un dossier.
+Il gére les erreurs 400 et 404 et gére plusieurs connexions.
+
+
+Comment utiliser le projet :
+
+Au niveau du lancement de l'application, une fois le projet importé sur Eclipse, il suffit de lancer le projet, suite à cela la console d’Eclipse va se lancer mais rien n'y apparaitra, ce qui est normal.
+Une fois que cela est lancer il suffit de se rendre sur une page internet quelconque, et d'inscrire dans la barre de recherche URL ce qui suit :"localhost :8080". 
+Suite à cela Notre index va se charger, présentant brièvement notre projet ainsi que les membres de notre groupe, pour enfin présenter en dessous la liste des sites accessible sur le serveur web que nous avons créé.
+
+Pour ce qui est de son utilisation a partir du fichier run.bat présent dans bindist-win/bin (coté Windows) et bindist-unix/bin (coté mac et linux), le lancer, exécute correctement el projet comme sur Eclipse,
+Mais contrairement à ce qui est mis en place, lorsque que nous cherchons localhost:8080 sur une page internet notre serveur n'est pas repéré et donc le chargement de la page index ne peut se faire.
+
+Ainsi pour être sûr de faire fonctionner le projet, le lancer sur Eclipse, après l'avoir importé, permet d'y accéder par la suite sur une page internet au choix.
+
+
+
+Liste des fonctionnalités manquantes :
+
+Dû aux nombreuses difficultés rencontrées de compréhension et de mise en place, ainsi qu'a l'emploi du temps assez chargés, deux fonctionnalités n'ont pu être implantées et sont les suivants :
+
+- gérer les erreurs 400 (les 404 ont-elles été gérées)
+- le fait de protéger une ressource par une authentification basique : le fichier contient des lignes sous la forme « username: password_en_md5 » 
+
+
+
+Fonctionnalités bonus choisie :
+
+Comme vous avez pu le constater précédemment, certaines fonctionnalités principales n'ont pu être implémenté. Nous étant concentrées sur le 
+fait de les implémentées d'abord, nous n'avons malheureusement pas pu nous lancer sur l'ajout d'une fonctionnalité bonus.
+
+
+
+Remarques supplémentaires :
+
+Nous avons beaucoup apprécié travailler sur ce projet qui nous a forcer à sortir de pas mal de zone de confort au niveau de nos connaissances. 
+Malgré les nombreux problèmes que nous avons rencontrés, ainsi que les problèmes pour se retrouver pour travailler à cause des emplois du temps très chargés et très différents de chacun, 
+nous sommes heureux du résultat que nous avons pu atteindre.
+
---
+++ src/main/java/fr/ul/miage/ReseauxProject/ReseauxServer.java
@@ -0,0 +1,89 @@
+package fr.ul.miage.ReseauxProject;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+// Read the full article https://dev.to/mateuszjarzyna/build-your-own-http-server-in-java-in-less-than-one-hour-only-get-method-2k02
+public class ReseauxServer {
+	public static final Logger LOG = Logger.getLogger(ReseauxServer.class.getName());
+
+    public static void main( String[] args ) throws Exception {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                try (Socket client = serverSocket.accept()) {
+                    handleClient(client);
+                }
+            }
+        }
+    }
+    
+    private static void sendResponse(Socket client, String status, String contentType, byte[] content) throws IOException {
+        OutputStream clientOutput = client.getOutputStream();
+        clientOutput.write(("HTTP/1.1 \r\n" + status).getBytes());
+        clientOutput.write(("ContentType: " + contentType + "\r\n").getBytes());
+        clientOutput.write("\r\n".getBytes());
+        clientOutput.write(content);
+        clientOutput.write("\r\n\r\n".getBytes());
+        clientOutput.flush();
+        client.close();
+    }
+
+    private static void handleClient(Socket client) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line + "\r\n");
+        }
+
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String method = requestLine[0];
+        String path = requestLine[1];
+        String version = requestLine[2];
+        String host = requestsLines[1].split(" ")[1];
+
+        List<String> headers = new ArrayList<>();
+        for (int h = 2; h < requestsLines.length; h++) {
+            String header = requestsLines[h];
+            headers.add(header);
+        }
+
+        String accessLog = String.format("Client %s, method %s, path %s, version %s, host %s, headers %s",
+                client.toString(), method, path, version, host, headers.toString());
+        LOG.info(accessLog);
+
+
+        Path filePath = getFilePath(path);
+        if (Files.exists(filePath)) {
+            // file exist
+            String contentType = guessContentType(filePath);
+            sendResponse(client, "200 OK", contentType, Files.readAllBytes(filePath));
+        } else {
+            // Gestion de l'erreur 404
+            byte[] notFoundContent = "<h1>Not found :(</h1>".getBytes();
+            sendResponse(client, "404 Not Found", "text/html", notFoundContent);
+        }
+
+    }
+    
+    private static String guessContentType(Path filePath) throws IOException {
+        return Files.probeContentType(filePath);
+    }
+
+    private static Path getFilePath(String path) {
+        if ("/".equals(path)) {
+        	LOG.info("fing !");
+            path = "/index.html";
+        }
+        return Paths.get("c:samples", path);
+    }
+}
